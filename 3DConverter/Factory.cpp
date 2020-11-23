b9b4825d0a2ef9459269818b2476cb4bf9ead614
Nadir <organicoman@yahoo.fr>
2020-11-23 14:19:06 +0100

    implemented factory class

--- 3DConverter/Factory.cpp
+++ 3DConverter/Factory.cpp
@@ -1,1 +1,21 @@
 #include "Factory.h"
+
+std::shared_ptr<conv::Serializer> conv::Factory::makeSerializer(const json& fileFormat)
+{
+	return std::make_shared<conv::Serializer>(fileFormat);
+}
+
+std::shared_ptr<conv::Serializer> conv::Factory::makeSerializer(const std::string& jsonFileName)
+{
+	return std::make_shared<conv::Serializer>(jsonFileName);
+}
+
+std::shared_ptr<conv::Deserializer> conv::Factory::makeDeserializer(const json& fileFormat)
+{
+	return std::make_shared<conv::Deserializer>(fileFormat);
+}
+
+std::shared_ptr<conv::Deserializer> conv::Factory::makeDeserializer(const std::string& jsonFileName)
+{
+	return std::make_shared<Deserializer>(jsonFileName);
+}
--- 3DConverter/include/Factory.h
+++ 3DConverter/include/Factory.h
@@ -2,16 +2,22 @@
 #ifndef FACTORY_H
 #define FACTORY_H
 //----------STL---------
+#include <memory>
+#include <string>
 
 //----------------------
 #include "Namespace.h"
+#include "Serializer.h"
+#include "Deserializer.h"
+
 
 struct conv::Factory
 {
-	virtual Serializer makeSerializer(const json& fileFormat) = 0 ;
-	virtual Deserializer makeDeserializer(const json& fileFormat) = 0 ;
+	std::shared_ptr<Serializer> makeSerializer(const json& fileFormat);
+	std::shared_ptr<Serializer> makeSerializer(const std::string& jsonFileName);
 
-	virtual ~Factory();
+	std::shared_ptr<Deserializer> makeDeserializer(const json& fileFormat);
+	std::shared_ptr<Deserializer> makeDeserializer(const std::string& jsonFileName);
 };
 
 #endif //FACTORY_H
